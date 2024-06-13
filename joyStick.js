da6abe54492a49f21503e82f5c58bdbf6e41e798
blackflypress <76153831+blackflypress@users.noreply.github.com>
2024-06-13 04:49:55 -0700

    Add files via upload

--- joyStick.js
+++ joyStick.js
@@ -1,66 +1,68 @@
-
-// BFP Joystick v.1.0
+// BFP Joystick v.2.0
 // Coded by Loroko 
 // Created: 6/10/2024
-// Last Updated: 6/10/2024
+// Last Updated: 6/12/2024
 // blackflypress.com 
 
-const tapThreshold = 150;
-const swipeThreshold = 100
+const threshold = 200;
 
-class Example extends Phaser.Scene{
+class JoyStick extends Phaser.Scene{
+  constructor(){
+    super('JoyStick');
+  }// end constructor 
 
-    constructor(){
-        super('Touchpad');
-      }// end constructor 
+  preload() {
+    this.load.image('touchOrigin', 'touchpad.png'); // load touchpad assets
+    this.load.image('touchCurrent', 'touchpad.png'); 
+    this.load.bitmapFont('p2', 'PressStart2P.png', 'PressStart2P.xml'); // load font
+  }// end preload
 
-      preload() {
-        this.load.image('touchOrigin', 'touchpad.png'); // load touchpad assets
-        this.load.image('touchCurrent', 'touchpad.png'); 
-        this.load.bitmapFont('p2', 'PressStart2P.png', 'PressStart2P.xml');
-      }// end preload
-
-      create() {
-        this.add.bitmapText(50, 25 , 'p2', `Analog Control`, 45).setOrigin(0,0).setTint(0x00ff00);
-        this.AngleText = this.add.bitmapText(50, 950 , 'p2', `Angle:`, 45).setOrigin(0,0).setTint(0x00ff00);
-        this.DistanceText = this.add.bitmapText(50, 1000 , 'p2', `Distance:`, 45).setOrigin(0,0).setTint(0x00ff00);
-        this.Origin = this.add.image(0, 0, 'touchOrigin'); // create touchpad assets
-        this.Current = this.add.image(0, 0, 'touchCurrent');
-        this.Origin.setScale(6);
-        this.Current.setScale(6);
-        this.Origin.alpha = 0; 
-        this.Current.alpha = 0;
-        }// end create
+  create() {
+    this.add.bitmapText(50, 25 , 'p2', `Analog Joystick`, 45).setOrigin(0,0).setTint(0x00ff00); // create UI
+    this.angleText = this.add.bitmapText(50, 900 , 'p2', `Angle:`, 45).setOrigin(0,0).setTint(0x00ff00);
+    this.forceText = this.add.bitmapText(50, 950 , 'p2', `Force:`, 45).setOrigin(0,0).setTint(0x00ff00);
+    this.distanceText = this.add.bitmapText(50, 1000 , 'p2', `Distance:`, 45).setOrigin(0,0).setTint(0x00ff00);
+    this.origin = this.add.image(0, 0, 'touchOrigin'); // create touchpad assets
+    this.current = this.add.image(0, 0, 'touchCurrent');
+    this.origin.setScale(10);
+    this.current.setScale(10);
+    this.origin.alpha = 0; 
+    this.current.alpha = 0;
+  }// end create
   
-        update() {
-          this.AngleText.setText('Angle: ' + this.Angle); // UI updates
-          this.DistanceText.setText('Distance: ' + this.Distance);
-            if (this.input.pointer1.active) { // handle analog control
-              this.Origin.alpha = .6; // increase opacity
-              this.Current.alpha = .6;
-              this.Origin.setPosition(this.input.pointer1.downX, this.input.pointer1.downY); // assign coordinates
-              this.Current.setPosition(this.input.pointer1.x, this.input.pointer1.y); 
-              this.Angle = Math.trunc(this.input.pointer1.getAngle() * 180/Math.PI); // get data
-              this.Distance = Math.trunc(this.input.pointer1.getDistance());
-            }// end if (this.leftPointer.active...
-      
-            if (!this.input.pointer1.active) {      // handle analog control end
-              this.Active = false;
-              this.Origin.alpha = 0; // reduce opacity
-              this.Current.alpha = 0;
-            }// end if (!this.movePointer.active...
-      
-            if (!this.input.pointer1.active) { // handle all touch end
-              this.touch = false;
-            }// end if (!this.gesturePointer.active...
+  update() {
+    this.angleText.setText('Angle: ' + this.angle); // UI updates
+    this.distanceText.setText('Force: ' + this.force);
+    this.forceText.setText('Distance: ' + this.distance);
 
-        }//end update
-    //   } //end Touchpad scene
-      
-}
+      if (this.input.pointer1.active) { // handle analog control
+        this.origin.alpha = .25; // increase opacity
+        this.current.alpha = .25;
+        this.origin.setPosition(this.input.pointer1.downX, this.input.pointer1.downY); // assign coordinates
+        this.current.setPosition(this.input.pointer1.x, this.input.pointer1.y); 
+        this.angle = Math.trunc(this.input.pointer1.getAngle() * 180/Math.PI); // get data
+        this.distance = Math.trunc(this.input.pointer1.getDistance());
+        this.distance = Phaser.Math.Clamp(this.distance, 0, threshold);
+        this.force = Math.trunc(this.distance / threshold*100);
+
+          if (this.distance == threshold) { // limit distance of current visually
+            Phaser.Math.RotateAroundDistance(this.current, this.origin.x, this.origin.y, 0, threshold);
+          }// end if (this.distance
+
+      }// end if (this.leftPointer.active...
+
+      if (!this.input.pointer1.active) { // handle analog control end
+        this.origin.alpha = 0; // reduce opacity
+        this.current.alpha = 0;
+      }// end if (!this.movePointer.active...
+
+  }// end update
+
+}// end class JoyStick
+
 const config = {
     type: Phaser.AUTO,
-    width: 2160,
+    width: 2376,
     height: 1080,
     backgroundColor: 0xacabaf,
     input: {
@@ -83,9 +85,8 @@ const config = {
         autoCenter: Phaser.Scale.CENTER_BOTH
     },// end scale
     scene: [
-        Example
+        JoyStick
     ]// end scene
-    // seed: [ (Date.now() * Math.random()).toString() ]
   }; // end config
 export default new Phaser.Game(config);
 
